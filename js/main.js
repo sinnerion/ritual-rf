8def7e3a5972ea9ce5a51136f10519509233c48c
Sinnerion <a.dergachow@gmail.com>
2016-08-22 22:21:10 +0300

    memo-item's and spevial-serv_item's height fixed

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    // Header fixing
+    /*$('#header').removeClass('header-fixed');
+    $(window).scroll(function(){
+        if($(this).scrollTop > 20){
+            $('#header').addClass('header-fixed');
+        } else {
+            $('#header').removeClass('header-fixed');
+        }
+    });*/
+
+    // Column height in
+    function heightLeveler(column) {
+        var blockHeight = 0;
+        column.each(function(){
+            var thisHeight = $(this).height();
+            if(thisHeight > blockHeight){
+                blockHeight = thisHeight;
+            }
+        });
+        column.height(blockHeight);
+    };
+    heightLeveler($('.special-serv_item'));
+    heightLeveler($('.memo-item'));
+
+});
