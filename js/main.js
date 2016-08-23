2fd0c6abb30b4ec15b498f9c257554e58abb0f2e
Sinnerion <a.dergachow@gmail.com>
2016-08-23 13:59:10 +0300

    all done; smooth transition added (!) It's time to fix this shit with demON

--- js/main.js
+++ js/main.js
@@ -9,6 +9,16 @@ $(document).ready(function(){
         }
     });*/
 
+    // Smooth transition of sections
+    $(document).ready(function() {
+        $("a.scrollto").click(function () {
+            elementClick = $(this).attr("href")
+            destination = $(elementClick).offset().top;
+            $("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
+            return false;
+        });
+    });
+
     // Column height in
     function heightLeveler(column) {
         var blockHeight = 0;
