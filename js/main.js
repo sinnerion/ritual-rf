651d54cce221d5125eeff36641b4a6854f900f6d
Sinnerion <a.dergachow@gmail.com>
2016-08-26 11:59:59 +0300

    memo-items are flexible; problem with special-serv_items

--- js/main.js
+++ js/main.js
@@ -38,4 +38,41 @@ $(document).ready(function(){
     // heightLeveler($('.funner-free_item'));
     // heightLeveler($('.up-guarantee_item'));
 
+    // Flexbox fix
+    ;( function( $, window, document, undefined )
+    {
+        'use strict';
+
+        var s = document.body || document.documentElement, s = s.style;
+        if( s.webkitFlexWrap == '' || s.msFlexWrap == '' || s.flexWrap == '' ) return true;
+
+        var $list       = $( '.equal-height' ),
+            $items      = $list.find( '.equal-height_item' ),
+            setHeights  = function()
+            {
+                $items.css( 'height', 'auto' );
+
+                var perRow = Math.floor( $list.width() / $items.width() );
+                if( perRow == null || perRow < 2 ) return true;
+
+                for( var i = 0, j = $items.length; i < j; i += perRow )
+                {
+                    var maxHeight   = 0,
+                        $row        = $items.slice( i, i + perRow );
+
+                    $row.each( function()
+                    {
+                        var itemHeight = parseInt( $( this ).outerHeight() );
+                        if ( itemHeight > maxHeight ) maxHeight = itemHeight;
+                    });
+                    $row.css( 'height', maxHeight );
+                }
+            };
+
+        setHeights();
+        $( window ).on( 'resize', setHeights );
+        $list.find( 'img' ).on( 'load', setHeights );
+
+    })( jQuery, window, document );
+
 });
